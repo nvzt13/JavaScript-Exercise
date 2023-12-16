00dd59cb949458151c4373e34b59969c3ee1f8f8
Nevzat Atalay <nevzatatalay79@gmail.com>
2023-12-16 22:36:10 +0300

    update day 26

--- days/day_26/app.js
+++ days/day_26/app.js
@@ -1,20 +1,62 @@
 const countryApiUrl = 'https://restcountries.com/v2/all';
-let countries= []
+const startButton = document.getElementById("start");
+const nyButton = document.getElementById("any");
+const sort = document.getElementById("sort");
+const input = document.getElementById("filter");
 
+let countries = [];
+let filteredCountries = [];
+
+addEventListener();
+
+function addEventListener() {
+ input.addEventListener("keyup", filter);
+}
 
 fetch(countryApiUrl)
-.then((response)=>response.json())
-.then(async (data)=>{
-  await data.forEach(element => {
-    countries.push(element.name)
-  })
-display(countries)
-})
+ .then((response) => response.json())
+ .then(async (data) => {
+    await data.forEach((element) => {
+      countries.push(element.name);
+      filteredCountries.push(element.name);
+    });
+    // display(filteredCountries);
+ });
 
+console.log(countries);
 
-function display(arr){
-  arr.map((ar)=>{
-    const li= `<li>${ar}</li>`
-    document.querySelector('#list').innerHTML += li
-  })
+function display(arr) {
+ const ul = document.querySelector('#list');
+ ul.innerHTML = '';
+
+ arr.map((ar) => {
+    const li = `<li>${ar}</li>`;
+    ul.innerHTML += li;
+ });
 }
+
+function filter(e) {
+ let filterValue = e.target.value;
+ let filter = [];
+
+ if (filterValue.length > 0) {
+    countries.forEach((country) => {
+      if (country.toLowerCase().includes(filterValue.toLowerCase())) {
+        filter.push(country);
+      }
+    });
+    filteredCountries = filter;
+ } else {
+    filteredCountries = countries;
+ }
+
+ display(filteredCountries);
+}
+
+// // Yeni işlev: Tüm ülkeleri göstermek için
+function displayAll() {
+ display(filteredCountries);
+}
+
+// // Varsayılan olarak tüm ülkeleri göster
+displayAll();
