9eaa5ae181069abba706c44f42190ca0933114d0
Nevzat Atalay <nevzatatalay79@gmail.com>
2023-12-17 13:38:49 +0300

    completed day 27

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const animation = document.getElementById("footer")
-
-const words = ["HTML", "CSS", "JavaScript", "React"];
+const header = document.getElementById("header")
+const headerWords=["Motivaional Speaker","Programmer", "Educator","Content Creator","Motivator","Instructer"]
+const footerWords = ["HTML", "CSS", "JavaScript", "React"];
 let i = 0; // bu değişkeni genel olarak tanımlamak gerekiyor
 
 function typeWord() {
-    if (i < words.length) {
-        let currentWord = words[i];
+    if (i < footerWords.length) {
+        let currentWord = footerWords[i];
         animation.innerHTML ="The tecnologies I teach  " + `<span>${currentWord}</span>`
         setTimeout(typeWord, 2000)
         i++
@@ -16,3 +17,17 @@ function typeWord() {
 }
 
 typeWord();
+
+function headerAnimation(){
+    if(i<headerWords.length){
+        let currentWord=headerWords[i]
+        header.innerHTML=currentWord
+       i++
+       setTimeout(headerAnimation,2000)
+    }
+    else{
+        i=0
+        headerAnimation()
+    }
+}
+headerAnimation()
