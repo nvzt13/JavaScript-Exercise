d6e16e8e3c751bebc04dccf104458ad609b104e4
Nevzat Atalay <nevzatatalay79@gmail.com>
2024-06-17 21:11:17 +0300

    update day 30 app js

--- days/day_30/app.js
+++ days/day_30/app.js
@@ -5,29 +5,55 @@ const input = document.getElementById("input");
 let countries = [];
 let searchCountries = [];
 
-// fill in countries array
-countriesData.forEach(element =>  {
-countries.push(element.name)
-});
-
 runEvents()
 
 // run events functions
 function runEvents() {
   input.addEventListener("keyup", searchCountry)
+
+}
+showCountries()
+// show countries in firsly
+function showCountries() {
+
+  countriesData.forEach((data) => {
+    let card =  `
+    <div class = "country-wrapper">
+    <img src = "${data.flag}">
+    <p class = "country-name">${data.name } </p>
+    <ul>
+    <li>Capital: ${data.capital} </li>
+    <li>Langue: ${data.langues} </li>
+    <li>Population: ${data.population} </li>
+    <ul>
+    </div>
+    `;
+      
+    main.innerHTML += card
+  })
 }
 
-// get data from user and fill in searchCountries array
-function searchCountry() {
 
+// fill in countries array
+countriesData.forEach(element =>  {
+countries.push(element.name)
+});
+
+
+// get data from user and fill in searchCountries array
+function searchCountry(e) {
+  
   searchCountries = [];
   main.innerHTML = "";
-  let inputValue = input.value.toLowerCase().trim();
-
+  let inputValue = e.target.value.toLowerCase().trim();
+ 
   countries.forEach((filterCountry) => {
     if(filterCountry.toLowerCase().trim().startsWith(inputValue) && 
     inputValue.length > 0) {  
       searchCountries.push(filterCountry)
+    }
+    else if (inputValue.length == 0){
+      showCountries()
     }
   })
   getInformationCountries()
@@ -39,9 +65,21 @@ function getInformationCountries() {
   searchCountries.forEach((count) => {
     countriesData.forEach((data) => {
       if(count.toLowerCase() == data.name.toLowerCase()) {
-        let card = new Country(data.name, data.flag, data.population,
-           data.langue, data.capital)
-           card.displayCountry()
+        // let card = new Country(data.name, data.flag, data.population,
+        //    data.langue, data.capital)
+        //    card.displayCountry()
+        let card =  `
+        <div class = "country-wrapper">
+        <img src = "${data.flag}">
+        <p class = "country-name">${data.name } </p>
+        <ul>
+        <li>Capital: ${data.capital} </li>
+        <li>Langue: ${data.langues} </li>
+        <li>Population: ${data.population} </li>
+        <ul>
+        </div>
+        `;
+         main.innerHTML += card
           }
         })
       })
@@ -49,27 +87,3 @@ function getInformationCountries() {
     
     
     
-    // class for countries show in html
-    class Country {
-      constructor(countryName, flag, population, langue, capital) {
-        this.countryName = countryName;
-        this.flag = flag;
-        this.population = population;
-        this.langue = langue;
-        this.capital = capital
-      }
-      displayCountry() {
-        const element = `
-        <div class = "country-wrapper">
-        <img src = "${this.flag}">
-        <p class = "country-name">${this.countryName} </p>
-        <ul>
-        <li>Capital: ${this.capital} </li>
-        <li>Langue: ${this.langue} </li>
-        <li>Population: ${this.population} </li>
-        <ul>
-        </div>
-        `;
-        main.innerHTML += element;
-      }
-    }
