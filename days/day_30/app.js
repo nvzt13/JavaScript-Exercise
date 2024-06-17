e66a876e49183d54e1d96280699b2cb4db9aa777
Nevzat Atalay <nevzatatalay79@gmail.com>
2024-06-17 17:13:15 +0300

    update day 30

--- days/day_30/app.js
+++ days/day_30/app.js
@@ -19,15 +19,18 @@ function runEvents() {
 
 // get data from user and fill in searchCountries array
 function searchCountry() {
+
+  searchCountries = [];
+  main.innerHTML = "";
   let inputValue = input.value.toLowerCase().trim();
 
   countries.forEach((filterCountry) => {
     if(filterCountry.toLowerCase().trim().startsWith(inputValue) && 
     inputValue.length > 0) {  
-       searchCountries.push(filterCountry)
-      }
-    })
-    getInformationCountries()
+      searchCountries.push(filterCountry)
+    }
+  })
+  getInformationCountries()
   }
 
 
@@ -36,37 +39,37 @@ function getInformationCountries() {
   searchCountries.forEach((count) => {
     countriesData.forEach((data) => {
       if(count.toLowerCase() == data.name.toLowerCase()) {
-     let card = new Country(data.name, data.flag, data.population,
+        let card = new Country(data.name, data.flag, data.population,
            data.langue, data.capital)
            card.displayCountry()
           }
-    })
-  })
-}
-
-
-
-// class for countries
-class Country {
-  constructor(countryName, flag, population, langue, capital) {
-    this.countryName = countryName;
-    this.flag = flag;
-    this.population = population;
-    this.langue = langue;
-    this.capital = capital
-  }
-  displayCountry() {
-    const element = `
-    <div class = "country-wrapper">
-    <img src = "${this.flag}">
-    <p class = "country-name">${this.countryName} </p>
-    <ul>
-    <li>Capital: ${this.capital} </li>
-    <li>Langue: ${this.langue} </li>
-    <li>Population: ${this.population} </li>
-    <ul>
-    </div>
-    `;
-    main.innerHTML += element
-  }
-}
+        })
+      })
+    }
+    
+    
+    
+    // class for countries show in html
+    class Country {
+      constructor(countryName, flag, population, langue, capital) {
+        this.countryName = countryName;
+        this.flag = flag;
+        this.population = population;
+        this.langue = langue;
+        this.capital = capital
+      }
+      displayCountry() {
+        const element = `
+        <div class = "country-wrapper">
+        <img src = "${this.flag}">
+        <p class = "country-name">${this.countryName} </p>
+        <ul>
+        <li>Capital: ${this.capital} </li>
+        <li>Langue: ${this.langue} </li>
+        <li>Population: ${this.population} </li>
+        <ul>
+        </div>
+        `;
+        main.innerHTML += element;
+      }
+    }
